854d96a8d32a4190db0847cf872f024904553019
MichaelFellas <MichaelAriti94@gmail.com>
2022-05-11 18:18:48 +0800

    TypeDefs done, Resolvers done, Mutations Done

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $bookId: String!) {
+    addSkill(userId: $userId, bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($userId: ID!, $bookId: String!) {
+    removeBook(userId: $userId, bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
