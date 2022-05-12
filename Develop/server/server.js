0416e89f79d5d3e25373a3678c51e3934a332394
MichaelFellas <MichaelAriti94@gmail.com>
2022-05-12 18:31:12 +0800

    login works, connects to apollo finally

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,31 +1,26 @@
 const express = require("express");
-// Import the ApolloServer class
 const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 
-// Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
-
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/public")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
